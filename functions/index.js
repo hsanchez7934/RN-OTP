f9991a898356e312817d709cea0922744198347f
Hector Sanchez <hsanchez7934@gmail.com>
2018-07-22 16:13:21 -0600

    add admin object

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require("./credentials.json");
+const hello
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
